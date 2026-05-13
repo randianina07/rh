1a3728f3d42e77efbda58698701cbfe703e4eaa7
keki-sun <rabotovaosundy@gmail.com>
2026-05-13 10:21:26 +0300

    filter

---
+++ app/Filters/RoleFilter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class RoleFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if (!session()->get('user')) {
+            return redirect()->to('/login');
+        }
+
+        $role = session()->get('role');
+
+        if (!in_array($role, $arguments)) {
+            return redirect()->to('/login');
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        //
+    }
+}
